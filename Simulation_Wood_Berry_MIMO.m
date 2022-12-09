040e3a8217b1d21ebe9506e3e9f522f206def4e1
borgesjose <jose.borges90@hotmail.com>
2022-12-09 16:30:11 -0300

    simulation-09-12-2022

---
+++ Simulation_Wood_Berry_MIMO.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+% Universidade Federal do Piauí                       %
+% Campus Ministro Petronio Portela                    %
+% Copyright 2022 -Jose Borges do Carmo Neto-          %
+% @author José Borges do Carmo Neto                   %
+% @email jose.borges90@hotmail.com                    %
+% simulation MIMO                                     %
+%                                                     %
+%  -- Version: 1.0  - 18/09/2022                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Coluna de destilação descrita em Wood e Berry (1973)
+
