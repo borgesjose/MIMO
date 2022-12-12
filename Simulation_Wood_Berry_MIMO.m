a386c94283fc22c9e29c18c9fda7a2e6275fc0dc
borgesjose <jose.borges90@hotmail.com>
2022-12-11 22:28:16 -0300

    11-12-2022

--- Simulation_Wood_Berry_MIMO.m
+++ Simulation_Wood_Berry_MIMO.m
@@ -12,7 +12,7 @@
 
 %% 1 - Tratando o processo:
 
-% Nesta etapa o processo � discretizado:
+% Nesta etapa o processo � discretizado:
 % Sendo:
 
 
@@ -50,29 +50,109 @@
     
     Gz = [Gz11,Gz12;Gz21,Gz22]
 %%
-
-Gz11
-[num den] = tfdata(Gz11, 'v')
-Gz11 = tf(num,den,Tc,'Variable','z^-1')
-
-Gz12
-[num den] = tfdata(Gz12, 'v')
-Gz12 = tf(num,den,Tc,'Variable','z^-1')
-
-Gz21
-[num den] = tfdata(Gz21, 'v')
-Gz21 = tf(num,den,Tc,'Variable','z^-1')
-
-Gz22
-[num den] = tfdata(Gz22, 'v')
-Gz22 = tf(num,den,Tc,'Variable','z^-1')
-
-%%
+figure
 step(G)
 hold on 
 step(Gz)
-    
-%%    
+hold off
+%%
 
+
+Gz11
+[num den] = tfdata(Gz11, 'v');
+Gz11 = tf(num,den,Tc,'Variable','z^-1')
+
+Gz12
+[num den] = tfdata(Gz12, 'v');
+Gz12 = tf(num,den,Tc,'Variable','z^-1')
+
+Gz21
+[num den] = tfdata(Gz21, 'v');
+Gz21 = tf(num,den,Tc,'Variable','z^-1')
+
+Gz22
+[num den] = tfdata(Gz22, 'v');
+Gz22 = tf(num,den,Tc,'Variable','z^-1')
+
+%%
+figure
+step(G)
+hold on 
+step(Gz)
+      
+%% Aplicando o controlador - OLD version
+% REF 1
+for i=1:nptos,
+    if (i<=nptos/2)  ref(i)=1; end;
+    if (i>nptos/2)   ref(i) = 2; end;
+end ;
+% REF 2
+for i=1:nptos,
+    if (i<=nptos/2)  ref(i)=1; end;
+    if (i>nptos/2)   ref(i) = 2; end;
+end ;
+
+y(4)=0 ; y(3)=0 ; y(2)=0 ; y(1)=0 ; 
+u(1)=0 ; u(2)=0 ; u(3)=0; u(4)=0;
+
+erro(1)=1 ; erro(2)=1 ; erro(3)=1; erro(4)=1;
+
+rlevel = 0.1;
+ruido = rlevel*rand(1,nptos);
+
+for i=5:nptos,
+
+P1(i) = p1+rlevel*rand; % Aplicando ruido na modelagem
+P2(i) = p2+ruido(i);  % Aplicando ruido na modelagem
+k = 2*P1(i)*P2(i); 
+    
+[c0,c1,c2,r0,r1,r2] = discretiza_zoh(P1(i),P2(i),k,Tc); %chama a função que discretiza o processo utilizano um ZOH;
+
+     if (i==550),r1 = - 1.84;r2 = 0.9109;  end % Ruptura no modelo
+     
+     y(i)= -r1*y(i-1)-r2*y(i-2)+c0*u(i-2)+c1*u(i-3)+c2*u(i-4); % equação da diferença do processo
+     
+     erro(i)=ref(i)-y(i); %Erro
+      
+      %Controlador:
+
+%             alpha = (Kc)*(1+((Td)/Tamostra)+(Tamostra/(2*(Ti))));
+%             beta = -(Kc)*(1+2*((Td)/Tamostra)-(Tamostra/(2*(Ti))));
+%             gama = (Kc)*(Td)/Tamostra;
+            
+      % new version
+            alpha = Kc+ Kd/Tamostra + (Ki*Tamostra)/2;
+            beta = -(Kc) - 2*((Kd)/Tamostra)+(Ki*Tamostra)/2;
+            gama = (Kd)/Tamostra;
+
+
+            u(i)= u(i-1) + alpha*erro(i) + beta*erro(i-1) + gama*erro(i-2);
+      
+       tempo(i)=i*Tamostra;
+      fprintf('amostra:  %d \t entrada:  %6.3f \t saida:  %4.0f\n',i,u(i),y(i));
+      
+ end ;
+ 
+ 
+     ISE_t2 = objfunc(erro,tempo,'ISE')
+     ITSE_t2 = objfunc(erro,tempo,'ITSE')
+     ITAE_t2 = objfunc(erro,tempo,'ITAE')
+     IAE_t2 = objfunc(erro,tempo,'IAE')
+     
+%plotar seinal de saida e  de controle:    
+figure;
+grid;
+plot(tempo,y,'g-');
+hold on;
+plot(tempo,u);
+plot(tempo,ref);
+title(['AT-PID-FG:',num2str(rlevel), ' ISE:', num2str(ISE_t2), ', ITSE:' ,num2str(ITSE_t2),', IAE:' ,num2str(IAE_t2), ', ITAE:' ,num2str(ITAE_t2)])
+%%
+%plotar P1 e P2
+figure;
+grid;
+plot(tempo,P1,'g-');
+hold on;
+plot(tempo,P2);
 
 
